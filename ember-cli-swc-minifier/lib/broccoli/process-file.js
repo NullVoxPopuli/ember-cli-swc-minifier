ef44457d932ba892adb61de9b8223aed9034185f
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-11-13 11:09:29 -0500

    feat(minifier): specify default options

--- ember-cli-swc-minifier/lib/broccoli/process-file.js
+++ ember-cli-swc-minifier/lib/broccoli/process-file.js
@@ -7,6 +7,24 @@ const debug = require('debug')('ember-cli-swc-minifier');
 const defaults = require('lodash.defaultsdeep');
 const swc = require('@swc/core');
 
+/**
+ * All options here:
+ *
+ * https://github.com/swc-project/swc/blob/b869c81888553b870a5a2c79c6ef49354df15670/node-swc/src/types.ts#L17
+ */
+const DEFAULT_OPTIONS = {
+  // Not exact implementation, cause these docs are the JS version, but:
+  // Explanations (and defaults) here: https://github.com/terser/terser#compress-options
+  // Rust implementation and defaults here: https://github.com/swc-project/swc/blob/2b2f6955f22c7ef04dd844e7aa686bbcefd977db/crates/swc_ecma_minifier/src/option/mod.rs#L96
+  compress: {
+    sequences: false,
+    reduce_funcs: true,
+  },
+  mangle: true,
+  // list of versions: https://github.com/swc-project/swc/blob/2b2f6955f22c7ef04dd844e7aa686bbcefd977db/crates/swc_ecma_minifier/src/option/terser.rs#L456
+  ecma: 2020,
+};
+
 module.exports = async function processFile(
   inFile,
   outFile,
@@ -30,13 +48,16 @@ module.exports = async function processFile(
     `outputPath`,
     `inlineSourcesContent`,
   ];
-  let options = Object.entries(defaults({}, _options)).reduce((result, [optionName, value]) => {
-    if (swcMinifyOptions.includes(optionName)) {
-      result[optionName] = value;
-    }
+  let options = Object.entries(defaults(_options, DEFAULT_OPTIONS)).reduce(
+    (result, [optionName, value]) => {
+      if (swcMinifyOptions.includes(optionName)) {
+        result[optionName] = value;
+      }
 
-    return result;
-  }, {});
+      return result;
+    },
+    {}
+  );
   let start = new Date();
 
   debug('[starting]: %s %dKB', relativePath, src.length / 1000);
@@ -49,24 +70,7 @@ module.exports = async function processFile(
      *
      * https://github.com/swc-project/swc/blob/b869c81888553b870a5a2c79c6ef49354df15670/node-swc/src/types.ts#L807
      */
-    let { code: outCode } = await swc.minify(src, {
-      /**
-       * All options here:
-       *
-       * https://github.com/swc-project/swc/blob/b869c81888553b870a5a2c79c6ef49354df15670/node-swc/src/types.ts#L17
-       */
-      compress: true, // TerserCompressOptions | boolean,
-      mangle: true, //TerserMangleOptions | boolean,
-      // ecma?: TerserEcmaVersion,
-      // keep_classnames?: boolean,
-      // keep_fnames?: boolean,
-      // module?: boolean,
-      // safari10?: boolean
-      // toplevel?: boolean
-      // outputPath: outFile,
-      // inlineSourcesContent?: boolean
-      ...options,
-    });
+    let { code: outCode } = await swc.minify(src, options);
 
     let end = new Date();
     let total = end - start;
