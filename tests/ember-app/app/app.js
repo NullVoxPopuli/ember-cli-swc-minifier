673dc7851c84e9a15418b4af20302c5492bd04a2
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-08 11:42:07 -0400

    lint:fix

--- tests/ember-app/app/app.js
+++ tests/ember-app/app/app.js
@@ -1,9 +1,9 @@
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 
 import config from 'ember-app/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
-import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
--- tests/ember-app/tests/test-helper.js
+++ tests/ember-app/tests/test-helper.js
@@ -1,8 +1,8 @@
 import { setApplication } from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
+import { setupEmberOnerrorValidation, start } from 'ember-qunit';
 import { loadTests } from 'ember-qunit/test-loader';
-import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 import Application from 'ember-app/app';
 import config from 'ember-app/config/environment';
